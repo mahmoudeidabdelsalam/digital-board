f5a4e7baf92bd043501e2163401aaca17f2e4847
Mahmoud Eid <mahmoudeidabdelsalam89@gmail.com>
2026-07-09 17:08:26 +0300

    add verification site to google

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,8 +5,8 @@ export const dynamic = 'force-static';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://example.com',
-      lastModified: new Date('2026-06-30'),
+      url: 'https://digital-board.pages.dev/',
+      lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
